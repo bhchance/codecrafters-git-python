73e31121bef40a4be85c3895e9e5b0c5c5a2c510
ryanchants <40925416+ryanchants@users.noreply.github.com>
2025-06-15 14:17:10 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,38 +9,37 @@ def main():
     # TODO: replace sys.argv with click
     command = sys.argv[1]
 
-    # CodeCrafters is using an ancient version of Python(3.9)
-    # Switch to match statement after finishing project and ejecting
-    if command == "init":
-        handle_init()
-        print("Initialized git directory")
-    elif command == "cat-file":
-        if sys.argv[2] != "-p":
-            # TODO: tighten up error handling of illegal flags. Easier after moving to click
-            # Only one TODO, applies to all entrypoints
-            ...
-        object_sha = sys.argv[3]
-        header, content = handle_read_file(object_sha)
-        print(content, end="")
+    match command:
+        case "init":
+            handle_init()
+            print("Initialized git directory")
 
-    elif command == "hash-object":
-        filename_to_hash = sys.argv[3]
-        write_file = sys.argv[2] == "-w"
-        object_hash = handle_hash_file(filename_to_hash, write_file)
-        print(object_hash)
+        case "cat-file":
+            if sys.argv[2] != "-p":
+                # TODO: tighten up error handling of illegal flags. Easier after moving to click
+                # Only one TODO, applies to all entrypoints
+                ...
+            object_sha = sys.argv[3]
+            header, content = handle_read_file(object_sha)
+            print(content, end="")
+        case "hash-object":
+            filename_to_hash = sys.argv[3]
+            write_file = sys.argv[2] == "-w"
+            object_hash = handle_hash_file(filename_to_hash, write_file)
+            print(object_hash)
 
-    elif command == "ls-tree":
-        if sys.argv[2] == "--name-only":
-            tree_sha = sys.argv[3]
-            _, nodes = handle_ls_tree(tree_sha, name_only=True)
-            for node in nodes:
-                print(node)
+        case "ls-tree":
+            if sys.argv[2] == "--name-only":
+                tree_sha = sys.argv[3]
+                _, nodes = handle_ls_tree(tree_sha, name_only=True)
+                for node in nodes:
+                    print(node)
 
-    elif command == "write-tree":
-        raise NotImplementedError
+        case "write-tree":
+            raise NotImplementedError
 
-    else:
-        raise RuntimeError(f"Unknown command #{command}")
+        case _:
+            raise RuntimeError(f"Unknown command #{command}")
 
 
 if __name__ == "__main__":
