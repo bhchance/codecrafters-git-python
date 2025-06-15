48a9ef021bedc277ae144d2e78f414af280a596c
ryanchants <40925416+ryanchants@users.noreply.github.com>
2025-06-15 11:28:30 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from pathlib import Path
 
 
 def main():
@@ -7,17 +8,17 @@ def main():
     print("Logs from your program will appear here!", file=sys.stderr)
 
     # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[1]
-    # if command == "init":
-    #     os.mkdir(".git")
-    #     os.mkdir(".git/objects")
-    #     os.mkdir(".git/refs")
-    #     with open(".git/HEAD", "w") as f:
-    #         f.write("ref: refs/heads/main\n")
-    #     print("Initialized git directory")
-    # else:
-    #     raise RuntimeError(f"Unknown command #{command}")
+
+    command = sys.argv[1]
+    if command == "init":
+        Path.mkdir(Path(".git"))
+        Path.mkdir(Path(".git/objects"))
+        Path.mkdir(Path(".git/refs"))
+        with open(".git/HEAD", "w") as f:
+            f.write("ref: refs/heads/main\n")
+        print("Initialized git directory")
+    else:
+        raise RuntimeError(f"Unknown command #{command}")
 
 
 if __name__ == "__main__":
