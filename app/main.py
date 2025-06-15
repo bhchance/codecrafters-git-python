6f9800b01ba7d68492e314d7524020f235487bd4
ryanchants <40925416+ryanchants@users.noreply.github.com>
2025-06-15 11:48:33 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,32 @@
 import sys
 import os
+import zlib
 from pathlib import Path
 
+git_objects_path = Path(".git/objects")
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
 
     command = sys.argv[1]
     if command == "init":
         Path.mkdir(Path(".git"))
-        Path.mkdir(Path(".git/objects"))
+        Path.mkdir(git_objects_path)
         Path.mkdir(Path(".git/refs"))
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    if command == "cat-file":
+        if sys.argv[2] != "-p":
+            #TODO: handle this
+            ...
+        object_hash = sys.argv[3]
+        folder = object_hash[:2]
+        filename = object_hash[2:]
+        with open(git_objects_path / folder / filename, "rb") as f:
+            data = zlib.decompress(f.read()).decode("utf-8")
+            header, content = data.split("\x00")
+            print(content, end="")
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
